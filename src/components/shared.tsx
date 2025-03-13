69725a9a84835acc1d242c194702d43131c1c123
HoangDuonng <Nguuyen1801@gmail.com>
2025-03-13 17:17:48 +0700

    chore(icon): fix bug not showing icon/image and config multi language description

--- src/components/shared.tsx
+++ src/components/shared.tsx
@@ -2,7 +2,7 @@
 
 const Images = {
     logo: '/assets/images/logo.png',
-    google: '/assets/images/Google.png',
+    google: '/assets/images/google.png',
     vietnam: '/assets/images/vietnamflag.png',
     england: '/assets/images/england.png',
 };
